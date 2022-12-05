4d1547d704f5c5d87911f4c19bbdf4e08209137c
Tranzzid <victor@blomkvist.me>
2022-12-05 15:34:49 +0100

    tailwind style

--- gik2f8-ht22-01-searchlist-p03/Components/BookDetails.js
+++ gik2f8-ht22-01-searchlist-p03/Components/BookDetails.js
@@ -3,13 +3,13 @@
 const BookDetails = (book) => {
     let html = 
     `
-    <div id = "bookDetail" class = "book-details position absolute bg-white/50 p-2 m-2 rounded w-1/5 top-1/4 ml-2 mx-auto flex flex-col text-center">
+    <div id = "bookDetail" class = "book-details bg-white/50 p-2 m-2 rounded w-1/5 ml-2 top-24 mx-auto flex flex-col text-center absolute ">
         <img class = "Image" src = "${book.coverImage}" alt = "Omslag saknas för denna bok. :(" >
 
-        <div class= "Border"> // border med hjälp av tailwind. </div>
+        <div class= "border-t-2 border-lime-600 py-3 my-3">
     
         <div class= "book_detail">
-            <ul class= "book_detail_list book-list rounded-md border-2 border-blue-400 bg-white w-full mx-auto">
+            <ul class= "book_detail_list book-list bg-white/50 rounded-md border-2 border-blue-400 bg-white w-full mx-auto">
                 <li class= "book_detail_list_item"> Author: ${book.author} </li>
                 <li class= "book_detail_list_item"> Title: ${book.title} </li>
                 <li class= "book_detail_list_item"> Pages: ${book.pages} </li>
@@ -22,3 +22,5 @@ const BookDetails = (book) => {
 
     return html;
 };
+
+// book-details bg-white/50 p-2 m-2 rounded w-1/5 top-1/4 ml-2 mx-auto flex flex-col text-center justify-end absolute 
